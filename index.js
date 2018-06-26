ebeee76853843abaa01fd72f307c1fdc1558fd79
Rebecca Kim <rebeccakim@rebeccas-mbp.coldspring.marian.edu>
2018-06-26 10:52:15 -0400

    custom header with form

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 //console.log('It works!')
 
+
+
 function updateHeader() {
     document.getElementById("secondary heading").innerHTML="it changed"
 }
+
+function updateCustom() {
+    const formChange = document.querySelector('.changeIsGood')
+    const formChangeText = formChange.value;
+    document.getElementById("thisHeading").innerHTML=formChangeText
+    event.preventDefault()
+}
+
+const button = addEventListener('submit',updateCustom)
+
+
 
 
