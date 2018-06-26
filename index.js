db9727c83b7802272be632523dc8124854ab5c69
Rebecca Kim <rebeccakim@rebeccas-mbp.coldspring.marian.edu>
2018-06-26 16:07:04 -0400

    makes text dissapear after written in form

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-//console.log('It works!')
-
-
-
 function updateHeader() {
-    document.getElementById("secondary heading").innerHTML="it changed"
+    document.getElementById("secondary heading").innerHTML="it changed" 
 }
 
 function updateCustom() {
-    const formChange = document.querySelector('.changeIsGood')
-    const formChangeText = formChange.value;
-    document.getElementById("thisHeading").innerHTML=formChangeText
-    event.preventDefault()
+    const formChange = document.querySelector('.changeIsGood') //gets the content of the textbox selected
+    const formChangeText = formChange.value; //store the text in a variable
+    document.getElementById("thisHeading").innerHTML=formChangeText //have the heading change
+    event.preventDefault() //prevents from refreshing
+    formChange.value='' //makes the text dissapear after written
+
 }
 
-const button = addEventListener('submit',updateCustom)
-
-
-
-
+const button = addEventListener('submit',updateCustom) //does the change
