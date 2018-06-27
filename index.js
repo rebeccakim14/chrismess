7913f5204422bf9ff3d93331f2f76daf8c7e2a13
Rebecca Kim <rebeccakim@rebeccas-mbp.coldspring.marian.edu>
2018-06-27 18:30:18 -0400

    have class notes and disabled other features for now

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-function updateHeader() {
+class App {
+    constructor() {
+        const form = document.querySelector('form#flickForm')
+        form.addEventListener('submit', (ev) => {
+            ev.preventDefault() //prevents from refreshing
+            this.updateFlicks(ev)
+        })
+    }
+
+/*updateHeader() {
     document.getElementById("secondary heading").innerHTML="it changed" 
 }
 
-function updateCustom() {
+updateCustom() {
     const formChange = document.querySelector('.changeIsGood') //gets the content of the textbox selected
     const formChangeText = formChange.value; //store the text in a variable
     document.getElementById("thisHeading").innerHTML=formChangeText //have the heading change
@@ -10,29 +19,47 @@ function updateCustom() {
     formChange.value='' //makes the text dissapear after written
 }
 const button = addEventListener('submit',updateCustom) //does the change
+*/
 
+renderProperty(name,value) {
+    const span = document.createElement('span')
+    span.classList.add(name)
+    span.textContent = value
+    return span
+}
 
-const form = document.querySelector('form#flickForm')
-function updateFlicks(ev) {
-    ev.preventDefault() //prevents from refreshing 
+renderItem(flick) {
+    const item = document.createElement('li')
+    item.classList.add('flick')
+
+    const properties = Object.keys(flick)
+
+    properties.forEach((propertyName) => {
+        const span = this.renderProperty(propertyName,flick[propertyName])
+        item.appendChild(span)
+    })
+    return item
+}
+
+updateFlicks(ev) {
     const f = ev.target //takes in parameter
+
+    const flick = {
+        name: f.flickName.value,
+        year: f.yearOfMovie.value,
+    }
   
-    const flickName = f.flickName.value //gets the content of the form
-    const item = document.createElement('li') //create a list element
-    item.textContent = flickName //gets the content
+    const item = this.renderItem(flick)
+    //item.textContent = flickName //gets the content
   
     const list = document.querySelector('#flicks') //finding the things with the content
     list.appendChild(item) //making a list 
 
-    const mYear = f.yearOfMovie.value //getting the value of the year
-    const item2 = document.createElement('li')
-    item2.textContent = mYear //text box content
-
-    list.appendChild(item2)
-
     f.reset() //after the whole function reset 
+    f.flickName.focus()
+  }
 }
-form.addEventListener('submit',updateFlicks)
+const app = new App()
 
 
 
