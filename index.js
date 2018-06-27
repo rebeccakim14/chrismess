e8ec18e9a6a990a62af35e1dfb62bf7301269549
Rebecca Kim <rebeccakim@rebeccas-mbp.coldspring.marian.edu>
2018-06-27 10:53:59 -0400

    flicks and movies can now be added into list

--- index.js
+++ index.js
@@ -9,7 +9,30 @@ function updateCustom() {
     event.preventDefault() //prevents from refreshing
     formChange.value='' //makes the text dissapear after written
 }
-
-
-
 const button = addEventListener('submit',updateCustom) //does the change
+
+
+const form = document.querySelector('form#flickForm')
+function updateFlicks(ev) {
+    ev.preventDefault() //prevents from refreshing 
+    const f = ev.target //takes in parameter
+  
+    const flickName = f.flickName.value //gets the content of the form
+    const item = document.createElement('li') //create a list element
+    item.textContent = flickName //gets the content
+  
+    const list = document.querySelector('#flicks') //finding the things with the content
+    list.appendChild(item) //making a list 
+
+    const mYear = f.yearOfMovie.value //getting the value of the year
+    const item2 = document.createElement('li')
+    item2.textContent = mYear //text box content
+
+    list.appendChild(item2)
+
+    f.reset() //after the whole function reset 
+}
+form.addEventListener('submit',updateFlicks)
+
+
+
